141c31394f44e11eec5b62c5ac3f21bc0aa3d5e6
jcaguirre-CL <jcaguirrecl@gmail.com>
2020-05-06 00:03:23 -0400

    cambios en la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from './login-containers/_services';
 import { User } from './login-containers/_models';
+
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 
 import './login-containers/_content/app.less';
 
@@ -11,35 +13,45 @@ import './login-containers/_content/app.less';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   currentUser: User;
   title = 'manager';
   navLinks: any[];
   activeLinkIndex = -1; 
 
-  panelOpenState = false;
+  panelOpenState = true;
 
-  constructor(private router: Router,
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+    private router: Router,
     private authenticationService: AuthenticationService
     ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     this.navLinks = [
         {
             label: 'INICIO',
             link: 'inicio',
-            index: 0
+            index: 1
         }, {
             label: 'REGISTRO',
             link: 'form1',
-            index: 1
+            index: 2
         }, {
             label: 'HISTORIAL',
             link: 'historial1',
-            index: 2
+            index: 3
         }, {
           label: 'CONFIGURACION',
           link: 'config1',
-          index: 3
+          index: 4
       } 
     ];
   }
@@ -52,4 +64,9 @@ export class AppComponent {
         this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
     });
   }
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = 'True';
 }
